b1481fc04cb3b50f2f7c4ea05e49b594ef05c05a
thinkphp <mergesortv@gmail.com>
2021-05-21 07:37:17 +0300

    projects

--- application/views/projects_view.php
+++ application/views/projects_view.php
@@ -26,6 +26,7 @@
 <body>
 <center>
 	<h1>./Projects</h1>	
+	
 	<?php
 	  echo"<p><mark>It works!</mark></p>";
 	  $a = 10;
@@ -40,7 +41,70 @@
               }
               return $a;
           }   
+          
+          function fib($n) {
+          
+                $arr = array();
+                $arr[0] = 0;
+                $arr[1] = 1;
+                $out = "<h3>Fibonacci: 0, 1, ";
+                for($i=2; $i <= $n; $i++) {
+                    $arr[$i] = $arr[$i-1] + $arr[$i-2];
+                    $out .= $arr[$i]. ", ";   
+                }          
+                $out .= "</h3>";
+                echo$out;
+          }
+          fib(50);
+          
+          function factorial($n, $t) {
+          
+                   if($n == 1) {
+                      return $t;
+                   } else {
+                      return factorial($n-1, $t * $n); 
+                   }
+          }
+          
+          $t =1;
+          
+          echo "<strong>Factorial(5) = </strong>", factorial(5,$t);
+          
+          function isPrime( $n ) {
+                $prime = True;
+                $i = 2;
+                while($i*$i <= $n && $prime == True) {
+                      $prime = $n % $i != 0; 
+                      $i+=1;  
+                }
+                return $prime;
+          }
+
+          function show_primes() {
+               $out = "<br/><h3>Primes: ";
+
+               $count = 0;
+               $i = 2;
+               while($count != 1000) {
+               
+                   if(isPrime($i)) {
+                      $out .= $i. ", ";
+                      $count += 1;
+                   } 
+                   $i += 1;
+               }               
+               $out .="</h3>"; 
+               echo$out;
+          }
+          show_primes();
          ?>
 </center>
+        <ul>
+	<li>Euclid's algorithm</li>
+	<li>Sequence Fibonacci</li>	
+        <li>Factorial with tail recursion</li>	
+        <li>Primes</li>	
+	</ul>
+
 </body>
 </html>
